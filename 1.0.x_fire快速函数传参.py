98149ee1c0968488e9a0e5e36301f095667c2104
ks1.zz <zz@ezaze.cn>
2021-11-14 21:48:09 +0800

    最简单的传参模块，功能测试

---
+++ 1.0.x_fire快速函数传参.py
@@ -0,0 +1,20 @@
+# 赋值功能测试
+# 同样是 1+2 可以通过传参实现
+# pip3.8 install fire
+# python example.py add 10 20
+
+import fire
+
+x = 1
+y = 2
+
+
+def add(x, y):
+    z = x + y
+    a = "计算 {0} + {1} 的结果：\n计算结果为{2}".format(x, y, z)
+    return a
+# a = "计算 {x} + {y} 的结果：\n计算结果为{z}".format(x=x,y=y,z=z)
+
+
+if __name__ == '__main__':
+    fire.Fire()
